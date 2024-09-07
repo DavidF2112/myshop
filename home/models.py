e776b366f5b7040442e4be5090632a559323bc32
David Rudenko <rudenkodavid2007@gmail.com>
2024-09-07 11:34:37 +0200

    надо добавить корзину и статику\

--- home/models.py
+++ home/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+        
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
-# Create your models here.
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+       return self.name
+    
+
+    def get_absolute_url(self):
+        return reverse('home:product_list_by_category',args=[self.slug])
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='product',
+                                 on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=50,db_index=True)    
+    slug = models.CharField(max_length=50,db_index=True,unique=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)  
+    description = models.TextField(max_length=100,blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uploaded = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+        ]
+
+    
+    def __str__(self):
+        return self.name    
+    
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.id , self.slug])
+    
--- shop/settings.py
+++ shop/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
